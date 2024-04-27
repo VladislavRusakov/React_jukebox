04edc71cb9ad8bf586f0cf81fdcbf6d1dd2d7fd0
VladislavRusakov <vladwebdev1612@gmail.com>
2024-04-27 14:20:19 +0400

    Start

---
+++ index.js
@@ -0,0 +1,219 @@
+const data =  [
+    {   
+        id: 1,
+        artist: "Casiopea",
+        title: "Mint Jams",
+        color: "white",
+        imageUrl: "https://i.pinimg.com/originals/82/1d/27/821d27facb9c9a81e60365778fc75dd7.jpg",
+        youtubeUrl: "https://www.youtube.com/watch?v=6GEI3PpXEAo"
+    },
+
+    {   
+        id: 2,
+        artist: "Ryo Fukui",
+        title: "Scenery",
+        color: "red",
+        imageUrl: "https://soulbrother.com/wp-content/uploads/2018/08/100784.jpg",
+        youtubeUrl: "https://www.youtube.com/watch?v=Hrr3dp7zRQY"
+    },
+
+    {   
+        id: 3,
+        artist: "Kindaichi Band",
+        title: "The adventure of Kohsuke Kindaichi",
+        color: "black",
+        imageUrl: "https://4.bp.blogspot.com/-y6c_SGehlm0/XAmxpvRV7JI/AAAAAAAG9J8/RjbgAb9AWBovhhuwZekSwBrYZs11hbCGgCLcBGAs/s1600/R-7686012-1446719846-5179.jpeg.jpg",
+        youtubeUrl: "https://www.youtube.com/watch?v=_-LZudHEk7k"
+    },
+
+    {   
+        id: 4,
+        artist: "Herbie Hancock",
+        title: "Head Hunters",
+        color: "blue",
+        imageUrl: "https://cdn1.ozone.ru/s3/multimedia-l/6359150493.jpg",
+        youtubeUrl: "https://youtu.be/3m3qOD-hhrQ"
+    },
+    
+    {   
+        id: 5,
+        artist: "Jiro Inagaki",
+        title: "Funky Stuff",
+        color: "black",
+        imageUrl: "https://sun9-72.userapi.com/impg/EMPib3qwajVChRnYyA-UKj8uUxgrrqkG6rdVmA/UjpKb8LIiAk.jpg?size=600x600&quality=95&sign=6f30fa2695ef573bc1ec4a119610821e&c_uniq_tag=5wsbPVXFqpZrw8AaW4HvW9nTIfwqRbzQ6sTixw8G--g&type=album",
+        youtubeUrl: "https://youtu.be/kjxxVkSd0XA"
+    },
+    
+    {   
+        id: 6,
+        artist: "Jiro Inagaki",
+        title: "In The Groove",
+        color: "white",
+        imageUrl: "https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/0e/41/a6/0e41a642-e9af-c601-8321-2fe94b232121/COKM-42677.jpg/1200x1200bf-60.jpg",
+        youtubeUrl: "https://www.youtube.com/watch?v=tJ6fyxjF-tU"
+    },
+    
+    {   
+        id: 7,
+        artist: "Cassiopea",
+        title: "Cassiopea",
+        color: "black",
+        imageUrl: "https://m.media-amazon.com/images/I/71oFtpSh4nL._UF1000,1000_QL80_DpWeblab_.jpg",
+        youtubeUrl: "https://www.youtube.com/watch?v=tZd4jjhLkWg"
+    },
+    
+    {   
+        id: 8,
+        artist: "Yuji Ohno",
+        title: "Cosmos",
+        color: "black",
+        imageUrl: "https://i.discogs.com/S6vsF-luktU1HhklocwukMhrqvPtDKXmxsmBg1uKrXw/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE0ODAy/NjItMTIyMjg1MTky/OC5qcGVn.jpeg",
+        youtubeUrl: "https://www.youtube.com/watch?v=np6zhvdwako"
+    },
+    
+    {   
+        id: 9,
+        artist: "Ryo Fukui",
+        title: "Mellow Dream",
+        color: "white",
+        imageUrl: "https://f4.bcbits.com/img/0012930481_71.jpg",
+        youtubeUrl: "https://www.youtube.com/watch?v=KxlvMmUjpTY"
+    },
+    
+    {   
+        id: 10,
+        artist: "Nucleus",
+        title: "Alleycat",
+        color: "yellow",
+        imageUrl: "https://is1-ssl.mzstatic.com/image/thumb/Music123/v4/59/db/63/59db6310-6fd5-0dae-7e55-2fd00c0a72d1/00602577651793.rgb.jpg/1200x1200bf-60.jpg",
+        youtubeUrl: "https://www.youtube.com/watch?v=giYvwLM-JEo"
+    },
+    
+    {   
+        id: 11,
+        artist: "Grover Washington",
+        title: "Feels So Good",
+        color: "blue",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/en/2/29/Feels_So_Good_Grover.jpeg",
+        youtubeUrl: "https://www.youtube.com/watch?v=520vLSJtuTg"
+    },
+    
+    {   
+        id: 12,
+        artist: "Grover Washington",
+        title: "A Secret Place",
+        color: "black",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/en/a/ab/ASecretPlace.JPG",
+        youtubeUrl: "https://www.youtube.com/watch?v=A-ESa_Nnv0s"
+    },
+    
+    {   
+        id: 13,
+        artist: "Grover Washington",
+        title: "Winelight",
+        color: "white",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/en/e/e7/Winelight.jpg",
+        youtubeUrl: "https://www.youtube.com/watch?v=zAxobRJVYEQ"
+    },
+    
+    {   
+        id: 14,
+        artist: "Grover Washington",
+        title: "Mister Magic",
+        color: "black",
+        imageUrl: "https://i.scdn.co/image/ab67616d0000b273dd15e56df5c95b6bb1c8e1d5",
+        youtubeUrl: "https://www.youtube.com/watch?v=EmucX5avn8M"
+    },
+    
+    {   
+        id: 15,
+        artist: "Jiro Inagaki",
+        title: "Blockbuster",
+        color: "white",
+        imageUrl: "https://e.snmc.io/i/1200/s/74ba03bbc757aad37f6b05a3698b6b09/2627318",
+        youtubeUrl: "https://www.youtube.com/watch?v=wzkL5SAEgi4"
+    },
+
+    {   
+        id: 16,
+        artist: "Himiko Kikuchi",
+        title: "Flying Beagle",
+        color: "black",
+        imageUrl: "https://cdsvinyljapan.com/cdn/shop/files/4547366657739.jpg?v=1707068805",
+        youtubeUrl: "https://youtu.be/HHOn8u-c2wk"
+    },
+
+    {   
+        id: 17,
+        artist: "Masayoshi Takanaka",
+        title: "An Insatiable High",
+        color: "white",
+        imageUrl: "https://i.discogs.com/9CNY1HNQZdu2I_58OOv6wi-TNPjjbzI15b4lYn6yOqU/rs:fit/g:sm/q:90/h:600/w:590/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI0MTYy/NTkwLTE2NjAxNjc3/NzAtNDMzNy5qcGVn.jpeg",
+        youtubeUrl: "https://www.youtube.com/watch?v=PQDGuUc-yrk"
+    },
+
+    {   
+        id: 18,
+        artist: "Masayoshi Takanaka",
+        title: "First Light",
+        color: "white",
+        imageUrl: "https://i.pinimg.com/736x/5c/b5/fb/5cb5fb947e3073189a6f8395872a89fa.jpg",
+        youtubeUrl: "https://youtu.be/o1rclw8Rugs"
+    },
+
+    {   
+        id: 19,
+        artist: "-",
+        title: "",
+        color: "",
+        imageUrl: "",
+        youtubeUrl: ""
+    },
+]
+
+function Shelf() {
+    let items = data.map(item => {
+        return (
+        <Book
+            key={item.id}
+            item={item}
+        />
+        )})
+
+    return (
+        <div className="shelf">
+            {items}
+        </div>
+    )
+}
+
+function Book(props) {
+    return (
+        <div className="book">
+          <div className="face spine white">
+            <div className="rotated-text">{props.item.artist}</div>
+          </div>
+            <div className="face cover">    
+                {
+                    props.item.youtubeUrl ?      
+                    <a href={props.item.youtubeUrl} target="_blank" rel="noopener noreferrer">
+                        <img src={props.item.imageUrl}/>
+                    </a>: 
+                    <a>
+                        <img src={props.item.imageUrl}/>
+                    </a>
+                }
+            </div>
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <div>
+        <Shelf />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
