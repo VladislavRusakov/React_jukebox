b3e11a86c0865eb1c298783b5baffb63a9f86c96
VladislavRusakov <vladwebdev1612@gmail.com>
2024-04-28 01:54:57 +0400

    Pics optimization and animated jukebox added

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const data =  [
         artist: "Casiopea",
         title: "Mint Jams",
         color: "white",
-        imageUrl: "https://i.pinimg.com/originals/82/1d/27/821d27facb9c9a81e60365778fc75dd7.jpg",
+        isPlayed: false,
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY_Vt_9uvqNdAD5dYjn0DIbqB0NafO8y5ogYRqw00GYA&s",
         youtubeUrl: "https://www.youtube.com/watch?v=6GEI3PpXEAo"
     },
 
@@ -13,6 +14,7 @@ const data =  [
         artist: "Ryo Fukui",
         title: "Scenery",
         color: "red",
+        isPlayed: false,
         imageUrl: "https://soulbrother.com/wp-content/uploads/2018/08/100784.jpg",
         youtubeUrl: "https://www.youtube.com/watch?v=Hrr3dp7zRQY"
     },
@@ -22,6 +24,7 @@ const data =  [
         artist: "Kindaichi Band",
         title: "The adventure of Kohsuke Kindaichi",
         color: "black",
+        isPlayed: false,
         imageUrl: "https://4.bp.blogspot.com/-y6c_SGehlm0/XAmxpvRV7JI/AAAAAAAG9J8/RjbgAb9AWBovhhuwZekSwBrYZs11hbCGgCLcBGAs/s1600/R-7686012-1446719846-5179.jpeg.jpg",
         youtubeUrl: "https://www.youtube.com/watch?v=_-LZudHEk7k"
     },
@@ -31,7 +34,8 @@ const data =  [
         artist: "Herbie Hancock",
         title: "Head Hunters",
         color: "blue",
-        imageUrl: "https://cdn1.ozone.ru/s3/multimedia-l/6359150493.jpg",
+        isPlayed: false,
+        imageUrl: "https://upload.wikimedia.org/wikipedia/en/5/54/Herbie-Hancock-Head-Hunters.png",
         youtubeUrl: "https://youtu.be/3m3qOD-hhrQ"
     },
     
@@ -40,6 +44,7 @@ const data =  [
         artist: "Jiro Inagaki",
         title: "Funky Stuff",
         color: "black",
+        isPlayed: false,
         imageUrl: "https://sun9-72.userapi.com/impg/EMPib3qwajVChRnYyA-UKj8uUxgrrqkG6rdVmA/UjpKb8LIiAk.jpg?size=600x600&quality=95&sign=6f30fa2695ef573bc1ec4a119610821e&c_uniq_tag=5wsbPVXFqpZrw8AaW4HvW9nTIfwqRbzQ6sTixw8G--g&type=album",
         youtubeUrl: "https://youtu.be/kjxxVkSd0XA"
     },
@@ -49,7 +54,8 @@ const data =  [
         artist: "Jiro Inagaki",
         title: "In The Groove",
         color: "white",
-        imageUrl: "https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/0e/41/a6/0e41a642-e9af-c601-8321-2fe94b232121/COKM-42677.jpg/1200x1200bf-60.jpg",
+        isPlayed: false,
+        imageUrl: "https://i.discogs.com/k4sOXtPJx3dxp57VxT86dJ5rNvSc9nuqY1EaV5Au2_Y/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE1Mjg4/ODI1LTE3MDg2MTA3/MzEtMzA0My5qcGVn.jpeg",
         youtubeUrl: "https://www.youtube.com/watch?v=tJ6fyxjF-tU"
     },
     
@@ -58,7 +64,8 @@ const data =  [
         artist: "Cassiopea",
         title: "Cassiopea",
         color: "black",
-        imageUrl: "https://m.media-amazon.com/images/I/71oFtpSh4nL._UF1000,1000_QL80_DpWeblab_.jpg",
+        isPlayed: false,
+        imageUrl: "https://upload.wikimedia.org/wikipedia/en/c/ca/CasiopeaSelfTitleAlbumCover.jpg",
         youtubeUrl: "https://www.youtube.com/watch?v=tZd4jjhLkWg"
     },
     
@@ -67,6 +74,7 @@ const data =  [
         artist: "Yuji Ohno",
         title: "Cosmos",
         color: "black",
+        isPlayed: false,
         imageUrl: "https://i.discogs.com/S6vsF-luktU1HhklocwukMhrqvPtDKXmxsmBg1uKrXw/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE0ODAy/NjItMTIyMjg1MTky/OC5qcGVn.jpeg",
         youtubeUrl: "https://www.youtube.com/watch?v=np6zhvdwako"
     },
@@ -76,6 +84,7 @@ const data =  [
         artist: "Ryo Fukui",
         title: "Mellow Dream",
         color: "white",
+        isPlayed: false,
         imageUrl: "https://f4.bcbits.com/img/0012930481_71.jpg",
         youtubeUrl: "https://www.youtube.com/watch?v=KxlvMmUjpTY"
     },
@@ -85,7 +94,8 @@ const data =  [
         artist: "Nucleus",
         title: "Alleycat",
         color: "yellow",
-        imageUrl: "https://is1-ssl.mzstatic.com/image/thumb/Music123/v4/59/db/63/59db6310-6fd5-0dae-7e55-2fd00c0a72d1/00602577651793.rgb.jpg/1200x1200bf-60.jpg",
+        isPlayed: false,
+        imageUrl: "https://i.discogs.com/Docl3MeGs-47HNncEzs6XIPSkkOLrSgafrbLFPlY9BA/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIzNjQw/NjM4LTE2NTU3NTQz/OTUtNDQ1OS5qcGVn.jpeg",
         youtubeUrl: "https://www.youtube.com/watch?v=giYvwLM-JEo"
     },
     
@@ -94,6 +104,7 @@ const data =  [
         artist: "Grover Washington",
         title: "Feels So Good",
         color: "blue",
+        isPlayed: false,
         imageUrl: "https://upload.wikimedia.org/wikipedia/en/2/29/Feels_So_Good_Grover.jpeg",
         youtubeUrl: "https://www.youtube.com/watch?v=520vLSJtuTg"
     },
@@ -103,6 +114,7 @@ const data =  [
         artist: "Grover Washington",
         title: "A Secret Place",
         color: "black",
+        isPlayed: false,
         imageUrl: "https://upload.wikimedia.org/wikipedia/en/a/ab/ASecretPlace.JPG",
         youtubeUrl: "https://www.youtube.com/watch?v=A-ESa_Nnv0s"
     },
@@ -112,6 +124,7 @@ const data =  [
         artist: "Grover Washington",
         title: "Winelight",
         color: "white",
+        isPlayed: false,
         imageUrl: "https://upload.wikimedia.org/wikipedia/en/e/e7/Winelight.jpg",
         youtubeUrl: "https://www.youtube.com/watch?v=zAxobRJVYEQ"
     },
@@ -121,6 +134,7 @@ const data =  [
         artist: "Grover Washington",
         title: "Mister Magic",
         color: "black",
+        isPlayed: false,
         imageUrl: "https://i.scdn.co/image/ab67616d0000b273dd15e56df5c95b6bb1c8e1d5",
         youtubeUrl: "https://www.youtube.com/watch?v=EmucX5avn8M"
     },
@@ -130,6 +144,7 @@ const data =  [
         artist: "Jiro Inagaki",
         title: "Blockbuster",
         color: "white",
+        isPlayed: false,
         imageUrl: "https://e.snmc.io/i/1200/s/74ba03bbc757aad37f6b05a3698b6b09/2627318",
         youtubeUrl: "https://www.youtube.com/watch?v=wzkL5SAEgi4"
     },
@@ -139,6 +154,7 @@ const data =  [
         artist: "Himiko Kikuchi",
         title: "Flying Beagle",
         color: "black",
+        isPlayed: false,
         imageUrl: "https://cdsvinyljapan.com/cdn/shop/files/4547366657739.jpg?v=1707068805",
         youtubeUrl: "https://youtu.be/HHOn8u-c2wk"
     },
@@ -148,6 +164,7 @@ const data =  [
         artist: "Masayoshi Takanaka",
         title: "An Insatiable High",
         color: "white",
+        isPlayed: false,
         imageUrl: "https://i.discogs.com/9CNY1HNQZdu2I_58OOv6wi-TNPjjbzI15b4lYn6yOqU/rs:fit/g:sm/q:90/h:600/w:590/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI0MTYy/NTkwLTE2NjAxNjc3/NzAtNDMzNy5qcGVn.jpeg",
         youtubeUrl: "https://www.youtube.com/watch?v=PQDGuUc-yrk"
     },
@@ -157,6 +174,7 @@ const data =  [
         artist: "Masayoshi Takanaka",
         title: "First Light",
         color: "white",
+        isPlayed: false,
         imageUrl: "https://i.pinimg.com/736x/5c/b5/fb/5cb5fb947e3073189a6f8395872a89fa.jpg",
         youtubeUrl: "https://youtu.be/o1rclw8Rugs"
     },
@@ -166,34 +184,61 @@ const data =  [
         artist: "-",
         title: "",
         color: "",
+        isPlayed: false,
         imageUrl: "",
         youtubeUrl: ""
     },
 ]
 
-function Shelf() {
+function Shelf(props) {
     let items = data.map(item => {
         return (
         <Book
             key={item.id}
             item={item}
+            handleClick={props.handleClick}
+            id={item.id}
+            isPlayed={item.isPlayed}
         />
         )})
 
     return (
+
         <div className="shelf">
             {items}
         </div>
     )
 }
 
-function Book(props) {
+function Jukebox(props) {
+    const playedBook = props.state.find((book) => book.isPlayed ? book.imageUrl: false)
+
     return (
-        <div className="book">
+    <div className="jukebox">
+        <div className="jukebox__head">
+            <div className="jukebox__record">
+                {playedBook ? <img className="jukebox__record-image" src={playedBook.imageUrl} />:
+                 <img className="jukebox__record-image" src="https://rms4.kufar.by/v1/list_thumbs_2x/adim1/0f5fb9d8-8f40-4ccc-af45-4aace9a6d7c8.jpg" />}
+            </div>
+        </div>
+        <div className="jukebox__body">
+            <div className="jukebox__internal">
+            </div>
+        </div>
+        <div className="jukebox__leg jukebox__leg--left"></div>
+        <div className="jukebox__leg jukebox__leg--right"></div>
+    </div>
+    )
+}
+
+function Book(props) {
+ 
+    return (
+        <div className="book" key={props.item.id}>
           <div className="face spine white">
             <div className="rotated-text">{props.item.artist}</div>
           </div>
-            <div className="face cover">    
+            <div className="face cover" onClick={()=>props.handleClick(props.id)}>    
                 {
                     props.item.youtubeUrl ?      
                     <a href={props.item.youtubeUrl} target="_blank" rel="noopener noreferrer">
@@ -209,9 +254,21 @@ function Book(props) {
 }
 
 function App() {
+    
+    const [nowPlaying, setNowPlaying] = React.useState(data)
+
+    function handleClick(id) {
+        setNowPlaying(prevNowPlaying => {
+            return prevNowPlaying.map((book) => {
+                return book.id === id ? {...book, isPlayed: true}: {...book, isPlayed: false}
+            })
+        })
+    }
+
     return (
         <div>
-        <Shelf />
+        <Shelf handleClick={handleClick}/>
+        <Jukebox state={nowPlaying}/>
         </div>
     )
 }
